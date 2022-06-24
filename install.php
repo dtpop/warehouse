1d966d46d3af612eefe2c75821489503d2bbf6b5
Wolfgang Bund <wb@dtp-net.de>
2022-06-24 11:31:59 +0200

    sql Installationsfehler

--- install.php
+++ install.php
@@ -8,10 +8,16 @@ $tables = [
 
 foreach ($tables as $table) {
     $content = rex_file::get(rex_path::addon('warehouse', 'install/tablesets/'.$table.'.json'));
+    $content = str_replace('"precision":""','"precision":"10"',$content);
     rex_yform_manager_table_api::importTablesets($content);
 }
+
 
 rex_sql_table::get(rex::getTable('wh_attributes'))
   ->ensureColumn(new rex_sql_column('pricemode', 'varchar(191)', false, ''))
   ->alter();
 
+
+rex_delete_cache();
+rex_yform_manager_table::deleteCache();
+  
